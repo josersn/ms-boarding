bc71f71550e38f603f43344fe643181e744fcb69
josersn <nettorammos@hotmail.com>
2023-02-11 21:18:10 -0300

    feat> delivery controller

---
+++ src/infrastructure/controllers/delivery.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, POST } from "fastify-decorators";
+import { FastifyReply } from "fastify/types/reply";
+import { OrderService } from "../../application/services/order.service";
+import { VolumeService } from "../../application/services/volume.service";
+import { CreateDeliveryUseCase } from "../../application/use-cases/delivery/create-delivery/create-delivery.use-case";
+import { OrderRepository } from "../../domain/repositories/repositories/order.repository";
+import { VolumeRepository } from "../../domain/repositories/repositories/volume.repository";
+
+@Controller("delivery")
+export default class DeliveryController {
+
+    @POST("/")
+    async createOrder(req, reply: FastifyReply) {
+        try {
+            const orderRepository = new OrderRepository();
+            const volumeRepository = new VolumeRepository();
+            const orderService = new OrderService(orderRepository);
+            const volumeService = new VolumeService(volumeRepository);
+            const useCase = new CreateDeliveryUseCase(orderService, volumeService);
+
+            const order = await useCase.exec(req.body);
+
+            return reply.status(201).send({
+                message: "Order created",
+                data: order
+
+            })
+
+        } catch (error) {
+            return reply.status(500).send({
+                error: true,
+                message: "Internal serve error"
+            })
+        }
+    }
+}
