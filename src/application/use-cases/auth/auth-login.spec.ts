16176e47a898969629aa72e53c583d80fd140b89
josersn <nettorammos@hotmail.com>
2023-02-15 20:44:11 -0300

    test: login tests

--- src/application/use-cases/auth/auth-login.spec.ts
+++ src/application/use-cases/auth/auth-login.spec.ts
@@ -6,6 +6,7 @@ import { Encryption } from "../../../infrastructure/adapters/encryption/implemen
 import { IEncryption } from "../../../infrastructure/adapters/encryption/interface";
 import { Tokenization } from "../../../infrastructure/adapters/tokenization/implementations";
 import { ITokenization } from "../../../infrastructure/adapters/tokenization/interfaces";
+import ApiError from "../../core/api-error";
 import { CompanyService, ICompanyService } from "../../services/company.service";
 import { IUserService, UserService } from "../../services/user.service";
 import { AuthLoginUserCase, IAuthLoginUseCase } from "./auth-login.use-case";
@@ -57,5 +58,25 @@ describe("Auth Login Test's", () => {
 
         expect(token).toBeTruthy();
 
-    })
+    });
+
+    it("Should not able to login with non exists user", async () => {
+
+        const data = {
+            email: "mock_user",
+            password: "mock_password"
+        };
+
+        await expect(useCase.exec(data)).rejects.toThrowError(new ApiError(404, 404, "User not found"));
+    });
+
+    it("Should not able to login with wrong password user", async () => {
+
+        const data = {
+            email: "user.mail@amazon.com.br",
+            password: "WRONG_PASSWORD"
+        };
+
+        await expect(useCase.exec(data)).rejects.toThrowError(new ApiError(404, 404, "User not found"));
+    });
 })
